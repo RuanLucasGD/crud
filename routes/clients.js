76f59ac9d4e863c55f8ad2a57f39d1fc3f4b8666
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2022-04-08 16:35:26 -0300

    improv: update & delete

--- routes/clients.js
+++ routes/clients.js
@@ -10,23 +10,38 @@ router.get('/', async function (req, res, next) {
   const conn = await db.connect();
   const clients = conn.collection("clients");
   const docs = await clients.find().toArray();
-  console.log(docs);
   res.render('list', { docs: docs, userAdded: false });
 });
 
 router.get('/add', async function (req, res, next) {
 
-  res.render('add');
+  res.render('add', { obj: { name: "", telephone: "", uf: "" }, label: "Adicionar" });
 });
 
-router.get('/edit/:id', async function (req, res, next) {
 
-  const id = req.params.id;
+router.post('/delete', async function (req, res, next) {
+
+  const id = req.body.hiddenId;
   const conn = await db.connect();
   const clients = conn.collection("clients");
-  const docs = await clients.find({ _id: ObjectID(id) }).toArray();
 
-  res.render('edit', { docs: docs });
+  await clients.deleteOne({ _id: new ObjectID(id) })
+
+  docs = await clients.find().toArray();
+
+  res.render('list', { docs: docs, userAdded: false })
+
+});
+
+router.post('/update', async function (req, res, next) {
+
+  const id = req.body.hiddenId;
+  const conn = await db.connect();
+  const clients = conn.collection("clients");
+
+  docs = await clients.findOne({ _id: new ObjectID(id) })
+
+  res.render('add', { obj: docs, label: "Atualizar" })
 });
 
 router.post('/', async function (req, res, next) {
@@ -36,14 +51,31 @@ router.post('/', async function (req, res, next) {
   const name = req.body.nome;
   const telephone = req.body.telefone;
   const uf = req.body.uf;
-  await clients.insertOne({
-    name: name,
-    telephone: telephone,
-    uf: uf
-  });
+
+  if (req.body.hiddenId) {
+
+    await clients.updateOne(
+      {
+        _id: new ObjectID(req.body.hiddenId)
+      },
+      {
+        $set: {
+          name: name,
+          telephone: telephone,
+          uf: uf
+        }
+      })
+  }
+  else {
+    await clients.insertOne({
+      name: name,
+      telephone: telephone,
+      uf: uf
+    });
+  }
 
   const docs = await clients.find().toArray();
-  res.render("list", { docs: docs, userAdded: true });
+  res.render("list", { docs: docs, userAdded: true, label: "Adicionar" });
 });
 
 module.exports = router;
